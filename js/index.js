6149b1f005b1f24d022703f83c8042a7377d3b02
MrFengShui <luansongjian@gmail.com>
2016-11-16 22:19:21 -0800

    update style and color

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
-function main() {
-    displayContents();
-    dynamicDateTime();
-}
-
 function displayContents() {
     var mainBox = document.getElementById('main-container');
 
-    for (var i = 0; i < 2; i ++) {
+    for (var i = 0; i < 4; i ++) {
         var contatiner = createMainSection(mainBox);
 
         for (var j = 0; j < 3; j ++) {
@@ -18,7 +13,6 @@ function displayContents() {
 function createMainSection(box, flag) {
     var section = document.createElement('section');
     section.setAttribute('id', 'main-container');
-    section.style.backgroundColor = flag ? "ghostwhite" : "#CF6766";
     box.appendChild(section);
 
     var container = document.createElement('div');
@@ -33,48 +27,12 @@ function createSectionTable(container, flag) {
     section.setAttribute('class', 'main-content-section');
 
     if (flag) {
-        section.style.backgroundColor = '#30415D';
+        section.style.backgroundColor = '#303030';
         section.style.borderColor = 'ghostwhite';
     } else {
         section.style.backgroundColor = 'ghostwhite';
-        section.style.borderColor = '#30415D';
+        section.style.borderColor = '#303030';
     }
 
     container.appendChild(section);
-}
-
-function dynamicDateTime() {
-	var date = new Date();
-	var year = date.getFullYear();
-	var month = date.getMonth() + 1;
-	var day = date.getDate();
-	var week = date.getDay();
-	var hour = date.getHours();
-	var minute = date.getMinutes();
-	var second = date.getSeconds();
-	document.getElementById("date-time").innerHTML = year + "-" + bitFormat(month) + "-" + bitFormat(day) + "\t" + weekFormat(week) + "\t" + bitFormat(hour) + ":" + bitFormat(minute) + ":" + bitFormat(second);
-	setTimeout(dynamicDateTime, 1000);
-}
-
-function bitFormat(num) {
-	return (num < 10) ? "0" + num : num;
-}
-
-function weekFormat(num) {
-	switch(num) {
-		case 0:
-			return "Sun";
-		case 1:
-			return "Mon";
-		case 2:
-			return "Tue";
-		case 3:
-			return "Wed";
-		case 4:
-			return "Thu";
-		case 5:
-			return "Fri";
-		case 6:
-			return "Sat";
-	}
 }
