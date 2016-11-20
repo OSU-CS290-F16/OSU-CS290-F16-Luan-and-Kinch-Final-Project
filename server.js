567dd059596974b9447d31a9079690d8fadcf1db
MrFengShui <luansongjian@gmail.com>
2016-11-20 09:47:22 -0800

    add four json files as database

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-var JSON = require('jsonfile');
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var db = require('./json/prog_db.json');
 
-JSON.readFile('package.json', function(err, data) {
-    console.log(data);
+var app = express();
+var port = process.env.PORT || 3000;
+
+var data = Object.keys(db);
+
+data.forEach(function(id) {
+    var tbFile = './json/' + id + '.json';
+    console.log(db[id]);
+    if(fs.existsSync(tbFile)) {
+        var tb = require(tbFile);
+    }
 });
