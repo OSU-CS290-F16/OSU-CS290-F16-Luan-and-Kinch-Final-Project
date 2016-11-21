0b4d661551cb7c0580f054e90a7ecbfe323fb362
MrFengShui <luansongjian@outlook.com>
2016-11-21 13:37:08 -0800

    add handlebars templates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-var db = require('./json/prog_db.json');
+var db = require('./public/json/prog_db.json');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -12,8 +12,29 @@ var data = Object.keys(db);
 
 data.forEach(function(id) {
     var tbFile = './json/' + id + '.json';
-    console.log(db[id]);
+
     if(fs.existsSync(tbFile)) {
         var tb = require(tbFile);
     }
+});
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.render('index-page', {
+        title: 'Welcome!'
+    });
+});
+
+app.get('*', function(req, res) {
+    res.status(404).render('404-page', {
+        title: '404 Error'
+    });
+});
+
+app.listen(port, function() {
+    console.log('Listening on Port: ', port);
 });
